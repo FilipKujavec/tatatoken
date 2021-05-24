069f7407e9c99ae6a80278c2e1f5f38f1958b4c7
Zuhayr Rashid <zuhayrr96@gmail.com>
2021-05-24 00:37:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import VariablesForm from "./VariablesForm";
+import "./styles.css";
+
+const initialState = {
+  holdings: '',
+  period: 1,
+  supply: 1000000,
+  volume: 0,
+  days: 7
+}
 
 function App() {
+  const [state, setState] = React.useState(() => initialState);
+
+  const apiKey = '7ZZ4WKZZIB8B71XYFV8Z1I1WD7B44GZCUJ'
+  const contract = '0x355389292D8c963719FDaF0651f7846D6c504448'
+
+  React.useEffect(() => {
+    fetch(`https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=${contract}&apikey=${apiKey}`)
+    .then(res => res.json())
+    .then((data) => {
+
+      const newSupply = Number(data.result/1000000000000000000000)
+
+      fetch(`https://api.coingecko.com/api/v3/coins/hakuna-metata/market_chart?vs_currency=usd&days=${state.days}&interval=daily`)
+      .then(res => res.json())
+      .then((data) => {
+        const volumes = data.total_volumes
+        const days = data.total_volumes.length
+        let volumeSum = 0
+        for (let i = 0; i < volumes.length; i++) {
+          volumeSum += volumes[i][1]
+        }
+        setState({ ...state, volume: Number((volumeSum/days).toFixed(2)), supply: Number(newSupply.toFixed(2)) })
+      })
+      .catch(console.log)
+
+    })
+    .catch(console.log)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="annualCompoundCalculator">
+      <h1 className="text-center"><span style={{color: 'orange'}}>$TATA</span> Earnings Calculator</h1>
+      <span>The figures given by this tool are only estimations. This calculator uses an n-day moving average of historical trade volume in order to predict potential earnings in the future.</span>
+      <hr />
+      <VariablesForm setState={setState} state={state} />
+      <hr />
+      <h1 style={{textAlign: 'center'}}>You earn <span style={{color: 'lime', fontSize: '48px'}}>${(((state.holdings/1000)/state.supply)*(state.volume*0.13)).toFixed(2)}</span> in a day</h1>
+      <span>In one day, you earn <span style={{color: 'lime', textDecoration: 'underline'}}>${(((state.holdings/1000)/state.supply)*(state.volume*0.13)).toFixed(2)}</span> of passive income on your <span style={{color: 'orange'}}>$TATA</span> holdings. The price of <span style={{color: 'orange'}}>$TATA</span> <span style={{fontWeight: 'bold', color: 'red'}}>DOES NOT MATTER</span> in this calculation. Whether people buy or sell, and whether the price goes up or down, you earn the same dollar amount on your holdings.<br/><br/><span style={{fontWeight: 'bold', color: 'red'}}>IN ADDITION</span>, your <span style={{color: 'orange'}}>$TATA</span> holdings grow by the amount you earn <span style={{color: 'lime', fontWeight: 'bold', textDecoration: 'underline'}}>DAILY</span>, which causes your future earnings to be <span style={{color: 'lime', textDecoration: 'underline', fontWeight: 'bold'}}>COMPOUNDED</span> and increase over time (assuming volume stays constant or grows).<br/><br/>This is the power of <span style={{color: 'orange'}}>$TATA</span>'s <span style={{color: 'lime', textDecoration: 'underline', fontWeight: 'bold'}}>REVOLUTIONARY TOKENOMICS</span> scheme. This is why it is more logical to <span style={{color: 'lime', textDecoration: 'underline', fontWeight: 'bold'}}>HODL</span> your tokens rather than sell them and <span style={{fontWeight: 'bold', color: 'red'}}>LOSE OUT ON PASSIVE INCOME</span>.</span>
     </div>
   );
 }
---
+++ src/VariablesForm.js
@@ -0,0 +1,97 @@
+import * as React from "react";
+
+function VariablesForm({ setState, state }) {
+
+  const { holdings, period, supply, volume, days } = state;
+
+  return (
+    <section>
+      <h2 style={{textAlign: 'center', marginBottom: '24px'}}>Calculate Your Passive Income</h2>
+      <div className="flex">
+        <label htmlFor="holdings">
+          Your $TATA Holdings (In Billions)
+          <input
+            className="variableInput"
+            type="number"
+            id="holdings"
+            name="holdings"
+            placeholder="Example: 100"
+            value={holdings}
+            onChange={({ target }) => setState({ ...state, holdings: Number(target.value) })}
+          />
+        </label>
+        <label htmlFor="period">
+          Projection Period (In Days)
+          <input
+            className="variableInput"
+            style={{background: 'orange'}}
+            type="text"
+            id="period"
+            name="period"
+            disabled
+            value={'Coming Soon!'}
+            onChange={({ target }) => setState({ ...state, period: Number(target.value) })}
+          />
+        </label>
+        <label htmlFor="supply">
+          Current $TATA Supply (In Trillions)
+          <input
+            className="variableInput"
+            style={{background: 'lightgrey'}}
+            type="number"
+            id="supply"
+            name="supply"
+            value={supply}
+            onChange={({ target }) => setState({ ...state, supply: Number(target.value) })}
+          />
+        </label>
+        <label htmlFor="volume">
+          Average Daily $TATA Volume (In USD)
+          <input
+            className="variableInput"
+            style={{background: 'lightgrey'}}
+            type="number"
+            id="volume"
+            name="volume"
+            value={volume}
+            onChange={({ target }) =>
+              setState({ ...state, volume: Number(target.value) })
+            }
+          />
+        </label>
+        <label htmlFor="days" className="slidecontainer">
+          Days To Average Daily Volume Over (7 Days Suggested)
+          <span className="rangeLabel">Average Of The Last {days} Days</span>
+          <input
+            type="range"
+            min="1"
+            max="7"
+            steps="1"
+            value={days}
+            className="slider"
+            id="days"
+            onChange={({ target }) => {
+              const daysSet = Number(target.value)
+
+              fetch(`https://api.coingecko.com/api/v3/coins/hakuna-metata/market_chart?vs_currency=usd&days=${daysSet}&interval=daily`)
+              .then(res => res.json())
+              .then((data) => {
+                const volumes = data.total_volumes
+                const days = data.total_volumes.length
+                let volumeSum = 0
+                for (let i = 0; i < volumes.length; i++) {
+                  volumeSum += volumes[i][1]
+                }
+                console.log(daysSet)
+                setState({ ...state, volume: Number((volumeSum/days).toFixed(2)), days: daysSet })
+              })
+              .catch(console.log)
+            }}
+          />
+        </label>
+      </div>
+    </section>
+  );
+}
+
+export default VariablesForm;
